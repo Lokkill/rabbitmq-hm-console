9af05970e7f8079ee40ec5efb6082fb538900779
Андрей Катаев <crovolk@yandex.ru>
2021-06-15 19:54:23 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/rabbitmq/console/consumer/ReceiverApp.java
@@ -0,0 +1,5 @@
+package ru.geekbrains.rabbitmq.console.consumer;
+
+public class ReceiverApp {
+
+}
---
+++ src/main/java/ru/geekbrains/rabbitmq/console/producer/SenderApp.java
@@ -0,0 +1,5 @@
+package ru.geekbrains.rabbitmq.console.producer;
+
+public class SenderApp {
+
+}
